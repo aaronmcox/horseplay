c008c7b818113b474732cd179949d285f0f9bb06
Aaron Cox <i.aaroncox@gmail.com>
2019-02-21 18:47:23 -0500

    move files into de directory

--- two.py
+++
@@ -1,9 +0,0 @@
-
-def reverseString(inputString):
-    return inputString[::-1]
-
-
-# if __name__ == "__main__":
-#     print(reverseString("apple"))
-#     print(reverseString("nope"))
-#     print(reverseString("1234"))
